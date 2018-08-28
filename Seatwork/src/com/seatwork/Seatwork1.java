414b62724d842785181419ecef50e1104af840c4
f.p.constantino <f.p.constantino@accenture.com>
2018-08-28 11:29:29 +0800

    Completed seatwork

--- Seatwork/src/com/seatwork/Seatwork1.java
+++ Seatwork/src/com/seatwork/Seatwork1.java
@@ -4,55 +4,66 @@ public class Seatwork1 {
 
 	public static void main(String[] args) {
 
-		for(int row = 1; row <= 7; row++) {
-			for(int i = 1; i <= row; i++) {
-				System.out.print("*");
-			}
-			System.out.println("");
-		}
+//		for(int row = 1; row <= 7; row++) {
+//			for(int i = 1; i <= row; i++) {
+//				System.out.print("*");
+//			}
+//			System.out.println("");
+//		}
+//
+//
+//		for(int row = 7; row > 0; row--) {
+//			for(int i = 0; i < row; i++) {
+//				System.out.print("*");
+//			}
+//			System.out.println("");
+//		}
 
+//		int j = 1;
+//		for(int row = 7; row > 1; row--) {
+//			System.out.print(j);
+//			j++;
+//			for(int i = 0; i < row - 2; i++) {
+//				System.out.print("*");
+//			}
+//			System.out.println("");
+//		}
 
-		for(int row = 7; row > 0; row--) {
-			for(int i = 0; i < row; i++) {
-				System.out.print("*");
-			}
-			System.out.println("");
-		}
-
-		int j = 1;
-		for(int row = 7; row > 0; row--) {
-			System.out.print(j);
-			j++;
-			for(int i = 0; i < row - 1; i++) {
-				System.out.print("*");
-			}
-			System.out.println("");
-		}
-
-		int k = 7;
-		for(int row = 7; row > 1; row--) {
-			for(int i = 1; i <= 7; i++) {
-
-				if(i >= k) {
-					System.out.print("&");
-				} else {
-					System.out.print(i);
-				}
-			}
-			k--;
-			System.out.println("");
-		}
-
-		for(int row = 7; row > 1; row--) {
-			for(int i = 1; i <= 7; i++) {
-				if(i >= row) {
-					System.out.print("*");
-				} else {
-					System.out.print(i);
-				}
-			}
-			System.out.println("");
-		}
+//		int k = 6;
+//		for(int row = 6; row > 0; row--) {
+//			for(int i = 1; i < 7; i++) {
+//
+//				if(i >= k) {
+//					System.out.print("&");
+//				} else {
+//					System.out.print(i);
+//				}
+//			}
+//			k--;
+//			System.out.println("");
+//		}
+//
+//		for(int row = 7; row > 1; row--) {
+//			for(int i = 1; i <= 7; i++) {
+//				if(i >= row) {
+//					System.out.print("*");
+//				} else {
+//					System.out.print(i);
+//				}
+//			}
+//			System.out.println("");
+//		}
+//
+//		for(int row = 1; row < 7; row++) {
+//			for(int i = 1; i < 7; i++) {
+//				if(i > row) {
+//					System.out.print("*");
+//				} else {
+//					System.out.print(i);
+//				}
+//			}
+//			System.out.println("");
+//		}
 
 
 	}
